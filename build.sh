86dce36590ab62365b10031ad2383fae3541c85e
lqh2307 <huylq23072000@gmail.com>
2024-09-22 02:12:08 +0700

    1.0.0

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# set -e  # Exit immediately if a command exits with a non-zero status
+
+# Set proxy
+# http_proxy=http://10.55.123.98:3333
+# https_proxy=http://10.55.123.98:3333
+
+# Set env
 export TARGET_ARCH=""
 export JAVA_VERSION="17"
 export GCC_ARCH="amd64"
@@ -13,23 +20,16 @@ export WITH_FILEGDB=""
 export WITH_PDFIUM="yes"
 export ARROW_VERSION="16.1.0-1"
 export ARROW_SOVERSION="1600"
-export PROJ_DATUMGRID_LATEST_LAST_MODIFIED=""
 export PROJ_INSTALL_PREFIX="/usr/local"
 export PROJ_VERSION="master"
 export GDAL_VERSION="master"
-export GDAL_RELEASE_DATE=""
-export GDAL_BUILD_IS_RELEASE=""
 export GDAL_REPOSITORY="OSGeo/gdal"
 export WITH_DEBUG_SYMBOLS="no"
-export RSYNC_REMOTE=""
-export WITH_PDFIUM="yes"
-export DEBIAN_FRONTEND="noninteractive"
 
-/home/ubuntu/gdal/docker/ubuntu-full/bh-set-envvars.sh
+./docker/ubuntu-full/bh-set-envvars.sh
 
-# Nếu TARGET_ARCH không rỗng, cập nhật danh sách sources
-if [ -n "$TARGET_ARCH" ]; then
-  rm -f /etc/apt/sources.list /etc/apt/sources.list.d/ubuntu.sources
+if [ -n "${TARGET_ARCH}" ]; then
+  rm -rf /etc/apt/sources.list /etc/apt/sources.list.d/ubuntu.sources
   echo "deb [arch=amd64] http://us.archive.ubuntu.com/ubuntu/ noble main restricted universe" >> /etc/apt/sources.list
   echo "deb [arch=amd64] http://us.archive.ubuntu.com/ubuntu/ noble-updates main restricted universe" >> /etc/apt/sources.list
   echo "deb [arch=amd64] http://us.archive.ubuntu.com/ubuntu/ noble-backports main restricted universe" >> /etc/apt/sources.list
@@ -38,139 +38,277 @@ if [ -n "$TARGET_ARCH" ]; then
   echo "deb [arch=${TARGET_ARCH}] http://ports.ubuntu.com/ubuntu-ports/ noble-updates main restricted universe" >> /etc/apt/sources.list
   echo "deb [arch=${TARGET_ARCH}] http://ports.ubuntu.com/ubuntu-ports/ noble-security main restricted universe" >> /etc/apt/sources.list
   dpkg --add-architecture ${TARGET_ARCH}
-  apt-get update -y
-  apt-get install -y g++-13-${GCC_ARCH}-linux-gnu
+  apt-get update -y && apt-get install -y \
+    g++-13-${GCC_ARCH}-linux-gnu
   ln -s ${GCC_ARCH}-linux-gnu-gcc-13 /usr/bin/${GCC_ARCH}-linux-gnu-gcc
   ln -s ${GCC_ARCH}-linux-gnu-g++-13 /usr/bin/${GCC_ARCH}-linux-gnu-g++
 fi
 
-# Setup build env for PROJ
-apt-get update -y
-apt-get install -y --fix-missing --no-install-recommends \
-  build-essential ca-certificates \
-  git make cmake wget unzip libtool automake \
-  zlib1g-dev${APT_ARCH_SUFFIX} libsqlite3-dev${APT_ARCH_SUFFIX} \
-  pkg-config sqlite3 libcurl4-openssl-dev${APT_ARCH_SUFFIX} \
-  libtiff-dev${APT_ARCH_SUFFIX}
+# Setup build env for PROJ & GDAL
+apt-get update -y && apt-get install -y \
+  libopenjp2-7-dev${APT_ARCH_SUFFIX} \
+  libcairo2-dev${APT_ARCH_SUFFIX} \
+  python3-dev${APT_ARCH_SUFFIX} \
+  python3-numpy${APT_ARCH_SUFFIX} \
+  python3-setuptools${APT_ARCH_SUFFIX} \
+  libpng-dev${APT_ARCH_SUFFIX} \
+  libjpeg-dev${APT_ARCH_SUFFIX} \
+  libgif-dev${APT_ARCH_SUFFIX} \
+  liblzma-dev${APT_ARCH_SUFFIX} \
+  libgeos-dev${APT_ARCH_SUFFIX} \
+  curl libxml2-dev${APT_ARCH_SUFFIX} \
+  libexpat-dev${APT_ARCH_SUFFIX} \
+  libxerces-c-dev${APT_ARCH_SUFFIX} \
+  libnetcdf-dev${APT_ARCH_SUFFIX} \
+  libpoppler-dev${APT_ARCH_SUFFIX} \
+  libpoppler-private-dev${APT_ARCH_SUFFIX} \
+  libspatialite-dev${APT_ARCH_SUFFIX} \
+  librasterlite2-dev${APT_ARCH_SUFFIX} \
+  libhdf4-alt-dev${APT_ARCH_SUFFIX} \
+  libhdf5-serial-dev${APT_ARCH_SUFFIX} \
+  libfreexl-dev${APT_ARCH_SUFFIX} \
+  unixodbc-dev${APT_ARCH_SUFFIX} \
+  mdbtools-dev${APT_ARCH_SUFFIX} \
+  libwebp-dev${APT_ARCH_SUFFIX} \
+  libpcre3-dev${APT_ARCH_SUFFIX} \
+  libcrypto++-dev${APT_ARCH_SUFFIX} \
+  libfyba-dev${APT_ARCH_SUFFIX} \
+  libkml-dev${APT_ARCH_SUFFIX} \
+  libmysqlclient-dev${APT_ARCH_SUFFIX} \
+  libogdi-dev${APT_ARCH_SUFFIX} \
+  libcfitsio-dev${APT_ARCH_SUFFIX} \
+  openjdk-"$JAVA_VERSION"-jdk${APT_ARCH_SUFFIX} \
+  libzstd-dev${APT_ARCH_SUFFIX} \
+  libpq-dev${APT_ARCH_SUFFIX} \
+  libssl-dev${APT_ARCH_SUFFIX} \
+  libboost-dev${APT_ARCH_SUFFIX} \
+  libarmadillo-dev${APT_ARCH_SUFFIX} \
+  libopenexr-dev${APT_ARCH_SUFFIX} \
+  libheif-dev${APT_ARCH_SUFFIX} \
+  libdeflate-dev${APT_ARCH_SUFFIX} \
+  libblosc-dev${APT_ARCH_SUFFIX} \
+  liblz4-dev${APT_ARCH_SUFFIX} \
+  libbz2-dev${APT_ARCH_SUFFIX} \
+  libbrotli-dev${APT_ARCH_SUFFIX} \
+  libarchive-dev${APT_ARCH_SUFFIX} \
+  libaec-dev${APT_ARCH_SUFFIX} \
+  libavif-dev${APT_ARCH_SUFFIX} \
+  libspdlog-dev${APT_ARCH_SUFFIX} \
+  libmagic-dev${APT_ARCH_SUFFIX} \
+  zlib1g-dev${APT_ARCH_SUFFIX} \
+  libsqlite3-dev${APT_ARCH_SUFFIX} \
+  libcurl4-openssl-dev${APT_ARCH_SUFFIX} \
+  libtiff-dev${APT_ARCH_SUFFIX} \
+  libgflags-dev${APT_ARCH_SUFFIX} \
+  swig ant liblcms2-2 \
+  autoconf automake bash-completion \
+  build-essential ca-certificates lsb-release \
+  pkg-config sqlite3 git cmake \
+  wget unzip libtool rsync ccache
 
-# Setup build env for GDAL
-apt-get install -y --fix-missing --no-install-recommends \
-  libopenjp2-7-dev libcairo2-dev python3-dev python3-numpy python3-setuptools \
-  libpng-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev \
-  libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev libpoppler-dev \
-  libspatialite-dev librasterlite2-dev swig ant libhdf4-alt-dev libhdf5-serial-dev \
-  libfreexl-dev unixodbc-dev mdbtools-dev libwebp-dev \
-  liblcms2-2 libpcre3-dev libcrypto++-dev libfyba-dev \
-  libkml-dev libmysqlclient-dev libogdi-dev \
-  libcfitsio-dev openjdk-${JAVA_VERSION}-jdk libzstd-dev \
-  libpq-dev libssl-dev libboost-dev autoconf automake bash-completion \
-  libarmadillo-dev libopenexr-dev libheif-dev libdeflate-dev libblosc-dev \
-  liblz4-dev libbz2-dev libbrotli-dev libarchive-dev libaec-dev libavif-dev
-
-# Tải và cài đặt KEA
+# Build likbkea
 wget -q https://github.com/ubarsc/kealib/archive/kealib-${KEA_VERSION}.zip
 unzip -q kealib-${KEA_VERSION}.zip
-rm -f kealib-${KEA_VERSION}.zip
 cd kealib-kealib-${KEA_VERSION}
-cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DHDF5_INCLUDE_DIR=/usr/include/hdf5/serial
+cmake . -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DHDF5_INCLUDE_DIR=/usr/include/hdf5/serial \
+  -DHDF5_LIB_PATH=/usr/lib/${GCC_ARCH}-linux-gnu/hdf5/serial \
+  -DLIBKEA_WITH_GDAL=OFF
 make -j$(nproc)
 make install
 cd ..
-rm -rf kealib-kealib-${KEA_VERSION}
+rm -rf kealib-kealib-${KEA_VERSION} kealib-${KEA_VERSION}.zip
 for i in /build_thirdparty/usr/lib/*; do strip -s $i 2>/dev/null || /bin/true; done
 for i in /build_thirdparty/usr/bin/*; do strip -s $i 2>/dev/null || /bin/true; done
 
-# Tải và cài đặt Mongo C Driver
-mkdir -p mongo-c-driver mongo-c-driver/build_cmake
-wget -q https://github.com/mongodb/mongo-c-driver/releases/download/${MONGO_C_DRIVER_VERSION}/mongo-c-driver-${MONGO_C_DRIVER_VERSION}.tar.gz -O - | tar xz -C mongo-c-driver --strip-components=1
-cd mongo-c-driver/build_cmake
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_TESTS=NO -DCMAKE_BUILD_TYPE=Release
+# Build mongo-c-driver
+wget -q https://github.com/mongodb/mongo-c-driver/releases/download/${MONGO_C_DRIVER_VERSION}/mongo-c-driver-${MONGO_C_DRIVER_VERSION}.tar.gz
+tar xzf mongo-c-driver-${MONGO_C_DRIVER_VERSION}.tar.gz
+cd mongo-c-driver-${MONGO_C_DRIVER_VERSION}
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DENABLE_TESTS=NO
 make -j$(nproc)
 make install DESTDIR=/build_thirdparty
 make install
 cd ../..
-rm -rf mongo-c-driver /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu/*.a
+rm -rf mongo-c-driver-${MONGO_C_DRIVER_VERSION} mongo-c-driver-${MONGO_C_DRIVER_VERSION}.tar.gz /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu/*.a
 for i in /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu/*; do strip -s $i 2>/dev/null || /bin/true; done
 for i in /build_thirdparty/usr/bin/*; do strip -s $i 2>/dev/null || /bin/true; done
 
-# Tải và cài đặt mongocxx
-mkdir -p mongocxx mongocxx/build_cmake
-wget -q https://github.com/mongodb/mongo-cxx-driver/archive/r${MONGOCXX_VERSION}.tar. -O - | tar xz -C mongocxx --strip-components=1
-cd mongocxx/build_cmake
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBSONCXX_POLY_USE_BOOST=ON -DENABLE_TESTS=OFF -DMONGOCXX_ENABLE_SLOW_TESTS=NO -DCMAKE_BUILD_TYPE=Release -DBUILD_VERSION=${MONGOCXX_VERSION}
+# Build mongocxx
+wget -q https://github.com/mongodb/mongo-cxx-driver/archive/r${MONGOCXX_VERSION}.tar.gz
+tar xzf r${MONGOCXX_VERSION}.tar.gz
+cd mongo-cxx-driver-r${MONGOCXX_VERSION}
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DBSONCXX_POLY_USE_BOOST=ON \
+  -DENABLE_TESTS=OFF \
+  -DMONGOCXX_ENABLE_SLOW_TESTS=NO \
+  -DBUILD_VERSION=${MONGOCXX_VERSION}
 make -j$(nproc)
 make install DESTDIR=/build_thirdparty
 make install
 cd ../..
-rm -rf mongocxx
+rm -rf mongo-cxx-driver-r${MONGOCXX_VERSION} r${MONGOCXX_VERSION}.tar.gz
 for i in /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu/*; do strip -s $i 2>/dev/null || /bin/true; done
 for i in /build_thirdparty/usr/bin/*; do strip -s $i 2>/dev/null || /bin/true; done
 
-# Tải và cài đặt TileDB
-mkdir -p tiledb
+# Build tiledb
 wget -q https://github.com/TileDB-Inc/TileDB/archive/${TILEDB_VERSION}.tar.gz
-tar xz -C tiledb --strip-components=1
-cd tiledb/build_cmake
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+tar xzf ${TILEDB_VERSION}.tar.gz
+cd TileDB-${TILEDB_VERSION}
+patch -p0 < ../docker/ubuntu-full/tiledb-FindLZ4_EP.cmake.patch
+patch -p0 < ../docker/ubuntu-full/tiledb-FindOpenSSL_EP.cmake.patch
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DTILEDB_WERROR=OFF \
+  -DTILEDB_SUPERBUILD=OFF \
+  -DTILEDB_TESTS=OFF \
+  -DCOMPILER_SUPPORTS_AVX2=FALSE \
+  -DOPENSSL_INCLUDE_DIR=/usr/include \
+  -DOPENSSL_CRYPTO_LIBRARY=/usr/lib/${GCC_ARCH}-linux-gnu/libcrypto.so \
+  -DOPENSSL_SSL_LIBRARY=/usr/lib/${GCC_ARCH}-linux-gnu/libssl.so
 make -j$(nproc)
-make install
+make -j$(nproc) install DESTDIR=/build_thirdparty
+make -j$(nproc) install
 cd ../..
+rm -rf TileDB-${TILEDB_VERSION} ${TILEDB_VERSION}.tar.gz
+for i in /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu/*; do strip -s $i 2>/dev/null || /bin/true; done
+for i in /build_thirdparty/usr/bin/*; do strip -s $i 2>/dev/null || /bin/true; done
 
-# Tải và cài đặt libOpenDRIVE
-if [ -n "$OPENDRIVE_VERSION" ]; then
-    wget -q https://github.com/DLR-TS/libOpenDRIVE/archive/refs/tags/${OPENDRIVE_VERSION}.tar.gz
-    tar xzf ${OPENDRIVE_VERSION}.tar.gz
-    rm -f ${OPENDRIVE_VERSION}.tar.gz
-    cd libOpenDRIVE-${OPENDRIVE_VERSION}
-    cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/
-    make -j$(nproc)
-    make install
-    cd ..
+# Build openjpeg
+if [ -n "${OPENJPEG_VERSION}" ]; then
+  wget -q https://github.com/uclouvain/openjpeg/archive/v${OPENJPEG_VERSION}.tar.gz
+  tar xzf v${OPENJPEG_VERSION}.tar.gz
+  cd openjpeg-${OPENJPEG_VERSION}
+  cmake . -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_STATIC_LIBS=OFF \
+    -DCMAKE_INSTALL_PREFIX=/usr
+  make -j$(nproc)
+  make install
+  mkdir -p /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu
+  rm -rf /usr/lib/${GCC_ARCH}-linux-gnu/libopenjp2.so*
+  mv -f /usr/lib/libopenjp2.so* /usr/lib/${GCC_ARCH}-linux-gnu
+  cp -P -rf /usr/lib/${GCC_ARCH}-linux-gnu/libopenjp2.so* /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu
+  for i in /build_thirdparty/usr/lib/${GCC_ARCH}-linux-gnu/*; do strip -s $i 2>/dev/null || /bin/true; done
+  cd ..
+  rm -rf openjpeg-${OPENJPEG_VERSION} v${OPENJPEG_VERSION}.tar.gz
 fi
 
-# Nếu cần, cài đặt File Geodatabase API
-if echo "$WITH_FILEGDB" | grep -Eiq "^(y(es)?|1|true)$"; then
-    wget -q https://github.com/Esri/file-geodatabase-api/raw/master/FileGDB_API_1.5.2/FileGDB_API-RHEL7-64gcc83.tar.gz
-    tar -xzf FileGDB_API-RHEL7-64gcc83.tar.gz
-    mv FileGDB_API-RHEL7-64gcc83 /usr/local/FileGDB_API
-    rm -rf /usr/local/FileGDB_API/lib/libstdc++*
-    cp /usr/local/FileGDB_API/lib/* /usr/lib/x86_64-linux-gnu
-    cp /usr/local/FileGDB_API/include/* /usr/include
-    rm -rf FileGDB_API-RHEL7-64gcc83.tar.gz
+# Build libOpenDRIVE
+if [ -n "${OPENDRIVE_VERSION}" ]; then
+  wget -q https://github.com/DLR-TS/libOpenDRIVE/archive/refs/tags/${OPENDRIVE_VERSION}.tar.gz
+  tar xzf ${OPENDRIVE_VERSION}.tar.gz
+  cd libOpenDRIVE-${OPENDRIVE_VERSION}
+  cmake . -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_INSTALL_PREFIX=/usr/
+  make -j$(nproc)
+  make install
+  mkdir -p /build_thirdparty/usr/lib
+  cp -P -rf /usr/lib/libOpenDrive*.so* /build_thirdparty/usr/lib
+  for i in /build_thirdparty/usr/lib/*; do strip -s $i 2>/dev/null || /bin/true; done
+  cd ..
+  rm -rf libOpenDRIVE-${OPENDRIVE_VERSION} ${OPENDRIVE_VERSION}.tar.gz
 fi
 
-# Cài đặt PDFium nếu cần
-if echo "$WITH_PDFIUM" | grep -Eiq "^(y(es)?|1|true)$"; then
+# Build File Geodatabase
+if echo "${WITH_FILEGDB}" | grep -Eiq "^(y(es)?|1|true)$"; then
+  wget -q https://github.com/Esri/file-geodatabase-api/raw/master/FileGDB_API_1.5.2/FileGDB_API-RHEL7-64gcc83.tar.gz
+  tar xzf FileGDB_API-RHEL7-64gcc83.tar.gz
+  mv -f FileGDB_API-RHEL7-64gcc83 /usr/local/FileGDB_API
+  rm -rf /usr/local/FileGDB_API/lib/libstdc++*
+  cp -rf /usr/local/FileGDB_API/lib/* /usr/lib/x86_64-linux-gnu
+  cp -rf /usr/local/FileGDB_API/include/* /usr/include
+  rm -rf FileGDB_API-RHEL7-64gcc83.tar.gz
+fi
+
+# Build libqb3
+git clone https://github.com/lucianpls/QB3.git --recursive
+cd QB3/QB3lib
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+  ${CMAKE_EXTRA_ARGS} \
+  -DCMAKE_INSTALL_PREFIX=/usr
+make -j$(nproc)
+make -j$(nproc) install
+make install DESTDIR=/build_thirdparty
+cd ../../..
+rm -rf QB3
+
+# Build PDFium
+if echo "${WITH_PDFIUM}" | grep -Eiq "^(y(es)?|1|true)$"; then
   wget -q https://github.com/rouault/pdfium_build_gdal_3_10/releases/download/pdfium_6677_v1/install-ubuntu2004-rev6677.tar.gz
-  tar -xzf install-ubuntu2004-rev6677.tar.gz
-  mv install/lib/* /usr/lib/
-  mv install/include/* /usr/include/
-  rm -rf install-ubuntu2004-rev6677.tar.gz install
-  apt-get update -y
-  apt-get install -y --fix-missing --no-install-recommends liblcms2-dev${APT_ARCH_SUFFIX}
+  tar xzf install-ubuntu2004-rev6677.tar.gz
+  rsync install/lib/ /usr/lib/
+  rsync install/include/ /usr/include/
+  rm -rf install install-ubuntu2004-rev6677.tar.gz
+  apt-get install -y \
+    liblcms2-dev${APT_ARCH_SUFFIX}
 fi
 
-# Cài đặt libjxl
-apt-get install -y libgflags-dev
+# Build libjxl
 git clone https://github.com/libjxl/libjxl.git --recursive
-cd libjxl/build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DJPEGXL_ENABLE_JPEGLI_LIBJPEG=OFF -DBUILD_TESTING=OFF -DJPEGXL_ENABLE_TOOLS=OFF -DJPEGXL_ENABLE_BENCHMARK=OFF ..
+cd libjxl
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DJPEGXL_ENABLE_JPEGLI_LIBJPEG=OFF \
+  -DBUILD_TESTING=OFF \
+  -DJPEGXL_ENABLE_TOOLS=OFF \
+  -DJPEGXL_ENABLE_BENCHMARK=OFF
 make -j$(nproc)
-make install
+make -j$(nproc) install
+make install DESTDIR=/build_thirdparty
 cd ../..
+rm -rf libjxl /lib/${GCC_ARCH}-linux-gnu/libjxl*.a /build_thirdparty/lib/${GCC_ARCH}-linux-gnu/libjxl*.a
 
-# Cài đặt Apache Arrow
-apt-get install -y lsb-release ca-certificates
+# Install Arrow C++
 wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-dpkg -i apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-apt-get update
-apt-get install -y libarrow${ARROW_SOVERSION} libparquet${ARROW_SOVERSION} libarrow-dataset${ARROW_SOVERSION} libarrow-dev=${ARROW_VERSION} libparquet-dev=${ARROW_VERSION} libarrow-acero-dev=${ARROW_VERSION} libarrow-dataset-dev=${ARROW_VERSION}
+dpkg -i \
+  apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+apt-get update && apt-get install -y \
+  libarrow${ARROW_SOVERSION}${APT_ARCH_SUFFIX} \
+  libparquet${ARROW_SOVERSION}${APT_ARCH_SUFFIX} \
+  libarrow-dataset${ARROW_SOVERSION}${APT_ARCH_SUFFIX} \
+  libarrow-dev${APT_ARCH_SUFFIX}=${ARROW_VERSION} \
+  libparquet-dev${APT_ARCH_SUFFIX}=${ARROW_VERSION} \
+  libarrow-acero-dev${APT_ARCH_SUFFIX}=${ARROW_VERSION} \
+  libarrow-dataset-dev${APT_ARCH_SUFFIX}=${ARROW_VERSION}
+rm -rf apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
 
-apt-get install -y libsqlite3-0 libtiff6 libcurl4 wget unzip ca-certificates
+# Install proj_grids & proj
+mkdir -p /tmp/proj_grids_build
+DESTDIR="/tmp/proj_grids_build" ./docker/ubuntu-full/bh-proj.sh
+LD_LIBRARY_PATH="/tmp/proj_grids_build" /tmp/proj_grids_build/usr/local/bin/projsync --target-dir /tmp/proj_grids --all
+rm -rf /tmp/proj_grids_build
+./docker/ubuntu-full/bh-proj.sh
 
-/home/ubuntu/gdal/docker/ubuntu-full/bh-proj.sh
+# Install gdal
+./docker/ubuntu-full/bh-gdal.sh
 
-/home/ubuntu/gdal/docker/ubuntu-full/bh-gdal.sh
+cp -rf /build_thirdparty/usr/ /usr/
 
-apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+cp -rf /tmp/proj_grids/* ${PROJ_INSTALL_PREFIX}/share/proj/
+
+cp -rf /build${PROJ_INSTALL_PREFIX}/share/proj/ ${PROJ_INSTALL_PREFIX}/share/proj/
+cp -rf /build${PROJ_INSTALL_PREFIX}/include/ ${PROJ_INSTALL_PREFIX}/include/
+cp -rf /build${PROJ_INSTALL_PREFIX}/bin/ ${PROJ_INSTALL_PREFIX}/bin/
+cp -rf /build${PROJ_INSTALL_PREFIX}/lib/ ${PROJ_INSTALL_PREFIX}/lib/
+
+cp -rf /build/usr/share/java /usr/share/java
+cp -rf /build/usr/share/gdal/ /usr/share/gdal/
+cp -rf /build/usr/include/ /usr/include/
+cp -rf /build_gdal_python/usr/ /usr/
+cp -rf /build_gdal_version_changing/usr/ /usr/
+
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+ldconfig
